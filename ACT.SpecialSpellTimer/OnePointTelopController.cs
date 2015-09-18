aaf461894c3a5bf14483aa18ce095f85eb394011
anoyetta <anoyetta@gmail.com>
2015-09-18 18:01:07 +0900

    整形

--- ACT.SpecialSpellTimer/OnePointTelopController.cs
+++ ACT.SpecialSpellTimer/OnePointTelopController.cs
@@ -2,9 +2,8 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq;
-    using System.Threading.Tasks;
+
     using ACT.SpecialSpellTimer.Properties;
     using ACT.SpecialSpellTimer.Sound;
     using ACT.SpecialSpellTimer.Utility;
